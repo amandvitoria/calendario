217389b85164a6c44fc5d7919427b52befd02405
Amnada vitoria <amandvitoria244@gmail.com>
2023-09-15 09:45:08 -0300

    alterações no calendario

--- calendar.js
+++ calendar.js
@@ -1,7 +1,13 @@
-function colorirData(){
+function colorirdata(){
+
     let days = document.getElementById('day').value;
+
     let color = document.getElementById('color').value;
+
     let calendar = document.getElementById('calendar');
-     let td      = calendar.getElementsByTagName('td')[parseInt(days)+5];
-     td.style.backgroundColor = color;
+
+    let td  = calendar.getElementsByTagName('td')[parseInt(days)+5];
+
+    td.style.backgroundColor = color;
+
 }
